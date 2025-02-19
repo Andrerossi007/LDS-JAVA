3368266afd61018f361583714364e86c98971426
André Rossi <andrerossi583@gmail.com>
2025-02-18 22:16:02 -0300

    feat: Configuracao do gitcomit

---
+++ src/main/java/br/fai/lds/Exercicio01.java
@@ -0,0 +1,13 @@
+package br.fai.lds;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        Exercicio01 app = new Exercicio01();
+        app.start();
+    }
+
+
+    private void start() {
+    }
+
+}
