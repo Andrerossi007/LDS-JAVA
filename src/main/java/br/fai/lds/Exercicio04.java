adc0bcc0c2ca8461b87f8b087672f2270708fe02
André Rossi <andrerossi583@gmail.com>
2025-02-21 14:17:21 -0300

    feat: criacao dos metodos para realizacao do exercicio

---
+++ src/main/java/br/fai/lds/Exercicio04.java
@@ -0,0 +1,12 @@
+package br.fai.lds;
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+        Exercicio04 app = new Exercicio04();
+        app.start();
+
+    }
+
+    private void start() {
+    }
+}
