2fa0fb249de51fdbc49e46f58c1bdbf7d260eecb
André Rossi <andrerossi583@gmail.com>
2025-02-21 15:49:32 -0300

    feat: Criacao dos metodo para resolucao do exercicio 5

---
+++ src/main/java/br/fai/lds/Exercicio05.java
@@ -0,0 +1,11 @@
+package br.fai.lds;
+
+public class Exercicio05 {
+    public static void main(String[] args) {
+        Exercicio05 app = new Exercicio05();
+        app.start();
+    }
+
+    private void start() {
+    }
+}
