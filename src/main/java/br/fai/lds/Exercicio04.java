25c16034cdfa508e82e2cbcaf9c6178a9c320a1c
André Rossi <andrerossi583@gmail.com>
2025-02-21 15:43:43 -0300

    feat: resolucao da questao 04

--- src/main/java/br/fai/lds/Exercicio04.java
+++ src/main/java/br/fai/lds/Exercicio04.java
@@ -1,5 +1,7 @@
 package br.fai.lds;
 
+import java.util.Scanner;
+
 public class Exercicio04 {
     public static void main(String[] args) {
         Exercicio04 app = new Exercicio04();
@@ -8,5 +10,24 @@ public class Exercicio04 {
     }
 
     private void start() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Frase: ");
+        String frase = scanner.nextLine().trim();
+
+        if (frase.isEmpty()){
+            System.out.println("Nada foi digitado.");
+        }else {
+            String[] palavra = frase.split("\\s+"); //Divide por espaco em branco
+
+            if (palavra.length > 1 && palavra.length <=4){
+                System.out.println("Penultima palavra: " + palavra[palavra.length -2]);
+            } else if (palavra.length > 4) {
+                System.out.println("Penultima Palavra: "+ palavra[palavra.length - 2] + " \nTexto muito longo");
+
+            }
+            
+        }
+        scanner.close();
     }
 }
